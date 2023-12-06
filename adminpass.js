b37664c63771474a0232b8305f7aee2c1e33d610
Samara Achini Karunanayaka <85082290+Samara-Karunanayaka@users.noreply.github.com>
2023-12-06 21:40:23 +0530

    Update adminpass.js

--- adminpass.js
+++ adminpass.js
@@ -9,3 +9,23 @@ const connection = mysql.createConnection({
   });
 
   //this code using for encrypt the admin passwords
+
+connection.query('SELECT * FROM admin', (error, results) => {
+    if (error) throw error;
+  
+    results.forEach((admin) => {
+      // Encrypt each password and update the table
+      bcrypt.hash(admin.password, 10, (err, hash) => {
+        if (err) throw err;
+  
+        connection.query(
+          'UPDATE admin SET password = ? WHERE a_id = ?',
+          [hash, admin.a_id],
+          (updateError, updateResults) => {
+            if (updateError) throw updateError;
+            console.log(`Password updated for admin with ID ${admin.a_id}`);
+          }
+        );
+      });
+    });
+  });
